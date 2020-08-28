6fcd2631ffeefe9ed37b2a7a2ce4827613a932ef
dharshakch97-cloud <saidh1971>
2020-08-28 16:56:48 +0530

    added list 'ul'

--- counter-app/src/Components/counter.jsx
+++ counter-app/src/Components/counter.jsx
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0
+        count: 1,
+        tags: ["tag1", "tag2", "tag3"]
      };
 
     render() { 
         return (
             <div>
-                <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </div>
         );
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
     }
 }
  
