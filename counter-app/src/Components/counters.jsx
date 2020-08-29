3c157108727863dbd75450b8acc00a5cdf1c022c
dharshakch97-cloud <saidh1971>
2020-08-29 12:01:22 +0530

    added delete func.

--- counter-app/src/Components/counters.jsx
+++ counter-app/src/Components/counters.jsx
@@ -9,10 +9,28 @@ class Counters extends Component {
             { id: 3, value: 2 },
         ]
      }
+
+     handleReset = () => {
+         const counters = this.state.counters.map(c => {
+             c.value = 0
+             return c
+         });
+         this.setState({ counters })
+     }
+     handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters })
+     };
+
     render() { 
         return (<div>
+            <button 
+                onClick={this.handleReset}
+                className="btn btn-primary btn-sm m-2">
+                    Reset
+            </button>
             { this.state.counters.map(counter => (
-                <Counter key={counter.id} value={counter.value} />
+                <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
             ))}
         </div>);
     }
