0cdb51550847a6cc1e5f51c6e2140d50a59edaaa
dharshakch97-cloud <saidh1971>
2020-08-29 12:52:52 +0530

    sfc in navbar

--- counter-app/src/Components/navbar.jsx
+++ counter-app/src/Components/navbar.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-    render() { 
-        return(
-            <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">
-                    Navbar <span className="badge badge-pill badge-secondary">
-                        {this.props.totalCounters}
-                    </span>
-                </span>
-            </nav>);
-    }
+//Stateless functional components
+const Navbar = (props) => {
+    return (<nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">
+            Navbar <span className="badge badge-pill badge-secondary">
+                {props.totalCounters}
+            </span>
+        </span>
+    </nav>);
 }
  
 export default Navbar;
