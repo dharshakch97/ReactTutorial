dddd4cd4633eb0641bd97836c0502d5b2fa39929
dharshakch97-cloud <saidh1971>
2020-08-29 12:49:10 +0530

    added navbar and count of itens

---
+++ counter-app/src/Components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() { 
+        return(
+            <nav className="navbar navbar-light bg-light">
+                <span className="navbar-brand mb-0 h1">
+                    Navbar <span className="badge badge-pill badge-secondary">
+                        {this.props.totalCounters}
+                    </span>
+                </span>
+            </nav>);
+    }
+}
+ 
+export default Navbar;
